ef19f5f8bebcf58ea56be4a64307b46d9cfcf49a
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2024-12-01 09:40:05 +0900

    add nix-direnv

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            nil
+            pyright
+          ];
+        };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
